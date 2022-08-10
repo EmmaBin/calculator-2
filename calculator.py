f6baa55fccaadc0becd7b4dcde77fc3cf0565e30
emmacodes <binma.emma@gmail.com>
2022-08-10 16:19:29 -0700

    finish tokenization and complete all the functions

--- calculator.py
+++ calculator.py
@@ -6,12 +6,31 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 # Replace this with your code
 while True:
+    #ask user for math questions
     user_choice= input("Type your math problem using operator, num1, num2 >>")
     tokens= user_choice.split(" ")
+    #quit if user input "q"
     if tokens[0] == "q":
         break
     else: 
         if tokens[0] == "+":
-            print(add(int(tokens[1]), int(tokens[2])))
+            print(add(float(tokens[1]), float(tokens[2])))
+        elif tokens[0] == "-":
+            print(subtract(float(tokens[1]), float(tokens[2])))
+        elif tokens[0] == "*":
+            print(multiply(float(tokens[1]), float(tokens[2])))
+        #divide the two arguments return the float value
+        elif tokens[0] == "/":
+            print(divide(float(tokens[1]), float(tokens[2])))
+        elif tokens[0] == "square":
+            print(square(float(tokens[1])))
+        elif tokens[0] == "cube":
+            print(cube(float(tokens[1])))
+        elif tokens[0] == "pow":
+            print(power((float(tokens[1])), float(tokens[2])))
+        elif tokens[0] == "mod":
+            print(mod(float(tokens[1]), float(tokens[2])))
+
+
 
 
